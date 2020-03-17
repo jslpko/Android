85bcb5fb56842700760a1b0df270f175fe2ba6e1
Usuario <luna6014@gmail.com>
2020-03-17 16:30:03 -0500

    Changes on Login test case beacuse the changes of V6

--- app/src/test/java/com/example/johan/appium/Login.java
+++ app/src/test/java/com/example/johan/appium/Login.java
@@ -1,6 +1,5 @@
 package com.example.johan.appium;
 
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -13,15 +12,13 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
-
 import io.appium.java_client.MobileBy;
 import io.appium.java_client.MobileElement;
 
-public class Rappi {
+public class Login {
     WebDriver driver;
 
     @Before
@@ -36,13 +33,13 @@ public class Rappi {
         capabilities.setCapability("app-wait-activity", "activity-to-wait-for");
 
         // Set android deviceName desired capability. Set your device name.
-        capabilities.setCapability("deviceName", "XT1562");
+        capabilities.setCapability("deviceName", "Moto G Play Build/NPIS26.48-43-2");
 
         // Set BROWSER_NAME desired capability. It's Android in our case here.
-        capabilities.setCapability(CapabilityType.BROWSER_NAME, "Android");
+        //capabilities.setCapability(CapabilityType.BROWSER_NAME, "Android");
 
         // Set android VERSION desired capability. Set your mobile device's OS version.
-        capabilities.setCapability(CapabilityType.VERSION, "7.0.0");
+        capabilities.setCapability(CapabilityType.VERSION, "7.1.1");
 
         // Set android platformName desired capability. It's Android in our case here.
         capabilities.setCapability("platformName", "Android");
@@ -53,14 +50,14 @@ public class Rappi {
         capabilities.setCapability("appPackage", "com.grability.rappi.dev");
 
         // Set android appActivity desired capability. It is
-        // com.android.calculator2.Calculator for calculator application.
+            // com.android.calculator2.Calculator for calculator application.
         // Set your application's appPackage if you are using any other app.
-        capabilities.setCapability("appActivity", "com.grability.app.activities.SplashActivity");
+        capabilities.setCapability("appActivity", "com.grability.app.splash.SplashActivity");
 
         // Created object of RemoteWebDriver will all set capabilities.
         // Set appium server address and port number in URL string.
         // It will launch calculator app in android device.
-        driver = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        driver = new RemoteWebDriver(new URL("http://0.0.0.0:58045/wd/hub"), capabilities);
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 
 
@@ -70,24 +67,27 @@ public class Rappi {
     public void testFirstLogin() {
 
         // Click on number 2 button.
-        driver.findElement(By.id("com.grability.rappi.dev:id/cardView_colombia")).click();
+        driver.findElement(By.id("com.grability.rappi.dev:id/textView_method")).click();
+        //driver.findElement(By.linkText("Con tu número celular")).click();
+        driver.findElement(By.id("com.grability.rappi.dev:id/editText_phone")).click();
 
-        driver.findElement(By.id("com.grability.rappi.dev:id/textView_email")).click();
 
         // Enter data in EditText
-        final WebElement inputField = driver.findElement(By.id("com.grability.rappi.dev:id/editText_email"));
-                inputField.sendKeys("johan.luna@rappi.com");
+        final WebElement inputField = driver.findElement(By.id("com.grability.rappi.dev:id/editText_phone"));
+                inputField.sendKeys("3133647701");
+                //I need a method here that close de keyboard
 
-        driver.findElement(By.id("com.grability.rappi.dev:id/editText_password")).click();
 
-        final WebElement inputField2 = driver.findElement(By.id("com.grability.rappi.dev:id/editText_password"));
-        inputField2.sendKeys("rappirappi");
+        driver.findElement(By.id("com.grability.rappi.dev:id/id/textView")).click();
 
-        driver.findElement(By.id("com.grability.rappi.dev:id/textView_email")).click();
+        driver.findElement(By.id("com.grability.rappi.dev:id/NO_ID")).click();
 
-        WebDriverWait wait = new WebDriverWait(driver, 1000000);
+        final WebElement inputField2 = driver.findElement(By.id("com.grability.rappi.dev:id/NO_ID"));
+        inputField2.sendKeys("123456");
+
+        WebDriverWait wait = new WebDriverWait(driver, 5000000);
         wait.until(ExpectedConditions.presenceOfElementLocated(By
-                .xpath("//android.widget.TextView[contains(@resource-id, 'com.grability.rappi.dev:id/textView_email')]")));
+                .xpath("//android.widget.TextView[contains(@resource-id, 'com.grability.rappi.dev:id/textView')]")));
 
         driver.manage().timeouts().implicitlyWait(1200,TimeUnit.SECONDS);
 
